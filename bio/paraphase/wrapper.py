b57e0ee513b14ca7c44099715a8453e46f81666e
magdalenazz <magdalenazz477@gmail.com>
2024-02-27 15:01:41 +0100

    feat: add

--- bio/paraphase/wrapper.py
+++ bio/paraphase/wrapper.py
@@ -6,20 +6,37 @@ __license__ = "MIT"
 from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
-
 extra = snakemake.params.get("extra", "")
+
 
 #annotate = snakemake.input.get("annotate", "")
 #if annotate:
 #    annotate = f"--annotate {annotate}"
 
 shell(
-    "cat  "
-    "{snakemake.input.bam} "
-    ">  {snakemake.output.vcf} "
-    "{extra} "
-    "{log}"
+    """
+    (TMPDIR={tmp_dir}; \
+    head  \
+    {snakemake.input.bam} \
+    >  {snakemake.output.vcf} \
+    ) {log}
+    """
 )
 
 
 
+
+#with tempfile.TemporaryDirectory(dir=tmp_root) as tmp_dir:
+#    shell(
+#        """
+#        (TMPDIR={tmp_dir}; \
+#        pbmm2 align --num-threads {snakemake.threads} \
+#            --preset {snakemake.params.preset} \
+#            --sample {snakemake.params.sample} \
+#            --log-level {snakemake.params.loglevel} \
+#            {extra} \
+#            {snakemake.input.reference} \
+#            {snakemake.input.query} \
+#            {snakemake.output.bam}) {log}
+#        """
+#    )
