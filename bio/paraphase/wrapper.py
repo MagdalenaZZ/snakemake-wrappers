cb2c91bb261bce9905f56531d7905e4e0ad43f96
magdalenazz <magdalenazz477@gmail.com>
2024-05-13 16:43:28 +0200

    feat: keet tmpdir for debugging

--- bio/paraphase/wrapper.py
+++ bio/paraphase/wrapper.py
@@ -14,7 +14,6 @@ extra = snakemake.params.get("extra", "")
 
 # Using mkdtemp() to create a temporary directory that is not automatically deleted
 tmpdirname = mkdtemp()
-print("TMPDIR: ",tmpdirname)
 
 try:
     shell(
@@ -29,9 +28,11 @@ try:
     )
 
     # Concatenating, reheadering, and sorting the zipped and indexed VCF files
-    #vcf_res = glob.glob(f"/{tmpdirname}/**/*variants.vcf", recursive=True)
+    bam_res = glob.glob(f"{tmpdirname}/*_realigned.bam")
+    shell("""
+    cp -pr bam_res {snakemake.output.bam}
+    """)
     vcf_res = glob.glob(f"{tmpdirname}/*_vcfs/*vcf")
-    print("RES: ", vcf_res )
     if vcf_res:
         for vcf in vcf_res:
             bgzip_cmd = f"bgzip -c {vcf} > {vcf}.gz"
